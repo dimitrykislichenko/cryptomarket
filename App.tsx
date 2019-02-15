144450510ecdb5663fdc1c4f996784af5053c766
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 16:00:45 +0200

    Add trigger that switch between storybook and app view

--- App.tsx
+++ App.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 import App from './src/app';
 import Storybook from './storybook';
 
+console.disableYellowBox = true;
+
+// Babel preset that should pass process.env.STORYBOOK
+// doesn't work,  don't have time to figure out why, so
+// just using const
+const STORYBOOK: boolean = false;
+
 const Root = () => {
-    if (process.env.STORYBOOK) {
+    if (STORYBOOK) {
         return <Storybook />;
     } else {
         return <App />;
