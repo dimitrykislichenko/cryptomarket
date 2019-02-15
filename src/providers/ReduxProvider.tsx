e12765aa37362dd265313598fad0dca56d64b687
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 15:56:38 +0200

    Add redux provider

---
+++ src/providers/ReduxProvider.tsx
@@ -0,0 +1,12 @@
+/**
+ * Setup redux provider with correct store
+ */
+
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import store from '../store';
+
+const ReduxProvider = ({ children }) => <Provider store={store}>{children}</Provider>;
+
+export default ReduxProvider;
