c2c70b083e11bf93d7b34d6653f4c5c243408fa5
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 15:59:27 +0200

    Load all required stories

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,10 +5,24 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+  require('../src/app/components/__stories__/AssetListItem.story');
+  require('../src/app/components/__stories__/Header.story');
+  require('../src/app/components/__stories__/TradesChart.story');
+  require('../src/common/components/__stories__/CryptoIcon.story');
+  require('../src/common/components/__stories__/ErrorMessage.story');
+  require('../src/common/components/__stories__/Label.story');
+  require('../src/common/components/__stories__/Spinner.story');
   
 }
 
 const stories = [
+  '../src/app/components/__stories__/AssetListItem.story',
+  '../src/app/components/__stories__/Header.story',
+  '../src/app/components/__stories__/TradesChart.story',
+  '../src/common/components/__stories__/CryptoIcon.story',
+  '../src/common/components/__stories__/ErrorMessage.story',
+  '../src/common/components/__stories__/Label.story',
+  '../src/common/components/__stories__/Spinner.story',
   
 ];
 
