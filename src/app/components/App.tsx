ba2c17d68b194d41dc065b0aba07d7b209f83df1
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 16:00:31 +0200

    Update application layout

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { ThemeProvider } from 'react-native-elements';
+import { ReduxProvider } from '../../providers';
+
+import Header from './Header';
+import AssetList from '../containers/AssetList';
 
 const App = () => (
-    <View>
-        <Text>Hello World!</Text>
-    </View>
+    <ReduxProvider>
+        <ThemeProvider>
+            <Header />
+            <AssetList />
+        </ThemeProvider>
+    </ReduxProvider>
 );
 
 export default App;
