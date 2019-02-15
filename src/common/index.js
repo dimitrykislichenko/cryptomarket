ee846318d929dd872475740aa599f972956cc5b1
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 15:58:37 +0200

    Re-export common components for convenience

---
+++ src/common/index.js
@@ -0,0 +1,4 @@
+export { default as CryptoIcon } from './components/CryptoIcon';
+export { default as Spinner } from './components/Spinner';
+export { default as ErrorMessage } from './components/ErrorMessage';
+export { default as Label } from './components/Label';
