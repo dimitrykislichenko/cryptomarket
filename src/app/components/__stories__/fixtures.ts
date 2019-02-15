8349092b31e7d61d6351e01f9dddfe1053cdf385
Dimitry Kislichenko <dimitrykislichenko@Dimitrys-iMac.local>
2019-02-15 15:56:23 +0200

    Add data fixtures

---
+++ src/app/components/__stories__/fixtures.ts
@@ -0,0 +1,24 @@
+export const AssetPair = {
+    altname: 'ADACAD',
+    wsname: 'ADA/CAD',
+    aclass_base: 'currency',
+    base: 'ADA',
+    l: ['0.052497', '0.051984'],
+    h: ['0.054000', '0.054000'],
+};
+
+export const Trades = {
+    labels: ['1', '2', '3', '4', '5', '6'],
+    datasets: [
+        {
+            data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+            ],
+        },
+    ],
+};
